b18e653e464010fabd0bafc6bc7dc4fe98c671f8
SondreB <sondre@outlook.com>
2022-11-10 02:15:03 +0100

    Add compatibility with existing web5 injectors
    - If the web5 is already registered but not of Web5 type, merge existing with Web5.

--- src/Injector.ts
+++ src/Injector.ts
@@ -9,11 +9,18 @@ export class Injector {
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		const global = globalThis as any;
 
-		if (typeof global !== 'undefined' && typeof global.web5 === 'undefined') {
+		if (typeof global.web5 === 'undefined') {
 			const web5 = new Web5();
 			web5.addProvider(provider);
 			global.web5 = web5;
 		} else {
+			// If the providers is not there, it's likely that the existing web5 object was not
+			// provided by our web5-injector but a wallet. Let's attempt to merge existing with
+			// the Web5.
+			if (!global.web5.providers) {
+				global.web5 = Object.assign(new Web5(), global.web5);
+			}
+
 			global.web5.addProvider(provider);
 		}
 	}
